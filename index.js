2721e4ea92941a7319c601e5d7c9a565cdcb1696
Ahmed-SD <ahmedadam.abduallah@gmail.com>
2022-08-10 12:33:20 -0700

    change uuid from variable to mehod to generate uuid in  every call

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+// load letters array from modules folder
 let letters = require("./modules/letters");
-let currentDate = Date.now();
-currentDate = currentDate.toString().split("");
-let uuid = "";
-let index = 0;
+let uuid = () => {
+  // create variable hold current time => aka: now in ml seconds
+  let currentDate = Date.now();
+  currentDate = currentDate.toString().split("");
+  let id = "";
+  let index = 0;
 
-for (let i = 0; i < letters.length; i++) {
-  let random = parseInt(Math.random() * letters.length - 1);
-  if (index == currentDate.length - 1) break;
-  if (i % 2 == 0) {
-    letters = letters.reverse();
+  for (let i = 0; i < letters.length; i++) {
+    let random = parseInt(Math.random() * letters.length - 1);
+    if (index == currentDate.length - 1) break;
+    if (i % 2 == 0) {
+      letters = letters.reverse();
+    }
+    id += `${letters[random]}${currentDate[index]}`;
+    index++;
   }
-  uuid += `${letters[random]}${currentDate[index]}`;
-  index++;
-}
+  return id;
+};
 module.exports = uuid;
